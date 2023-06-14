91d4ad531a180f4a667810b839c38acf49521eae
faraygd <aldifahriziilmawan@gmail.com>
2023-06-14 16:31:49 +0700

    update chain

--- const/mydetails.jsx
+++ const/mydetails.jsx
@@ -1,8 +1,8 @@
 
-export const contractAddress = "0x16697DC981432c5012C9b2b19CF25C985e3A81A6";
+export const contractAddress = "0xc0A426810ba6557919C53F61752870bBb08e9ee0";
 export const typeNFT="edition-drop";
 export const tokenId = 0;
-export const chainId = "arbitrum";
+export const chainId = "mumbai";
 export const truncateAddress = (address) => {
   return (
     address.substring(0, 6) + "..." + address.substring(address.length - 4)
